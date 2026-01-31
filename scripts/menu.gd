8391eb66a04ebcbc626d02e6b744bf369cdd931d
Simonas Senulis <simonas.senulis@gmail.com>
2026-01-31 22:46:23 +0200

    added audio sounds and improoved menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@onready var butt1 = $Butt
+@onready var butt2 = $Butt2
+
+var t = 0.0
+
+func _process(delta: float) -> void:
+	t += delta
+	butt1.scale = Vector2(1,1) * (1 + 0.05 * sin(t*6))
+	butt2.scale = Vector2(1,1) * (1 + 0.05 * sin(t*6))
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/coolTransition.tscn")
 
